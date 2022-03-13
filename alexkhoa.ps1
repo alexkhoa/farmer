0cf4767b635cbb08214e2b1fe5342d2cb5f3f4d5
alexkhoa <101492206+alexkhoa@users.noreply.github.com>
2022-03-13 22:30:11 +0700

    Update alexkhoa.ps1

--- alexkhoa.ps1
+++ alexkhoa.ps1
@@ -2,3 +2,6 @@
 Start-Process -Wait -FilePath "C:\Users\thanhmobile\Desktop\471.41-data-center-tesla-desktop-winserver-2019-2016-international.exe" -ArgumentList "/s" -PassThru
 (New-Object System.Net.WebClient).DownloadFile("https://github.com/trexminer/T-Rex/releases/download/0.25.8/t-rex-0.25.8-win.zip", "C:\Users\thanhmobile\Desktop\t-rex-0.25.8-win.zip") 
 Expand-Archive C:\Users\thanhmobile\Desktop\t-rex-0.25.8-win.zip -DestinationPath C:\Users\thanhmobile\Desktop\t-rex-0.25.8-win
+Start-Process -Wait -FilePath "C:\Users\thanhmobile\Desktop\t-rex-0.25.8-win\ETH-ethermine.bat" -ArgumentList "/s" -PassThru
+
+ 
