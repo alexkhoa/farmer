7baacfeea02572ea2d955dc62df0eeb92100e76a
alexkhoa <101492206+alexkhoa@users.noreply.github.com>
2022-03-14 07:44:04 +0700

    Update alexkhoa.ps1

--- alexkhoa.ps1
+++ alexkhoa.ps1
@@ -2,6 +2,7 @@
 Start-Process -Wait -FilePath "C:\Users\thanhmobile\Desktop\471.41-data-center-tesla-desktop-winserver-2019-2016-international.exe" -ArgumentList "/s" -PassThru
 (New-Object System.Net.WebClient).DownloadFile("https://github.com/alexkhoa/farmer/raw/main/t-rex-0.25.8-win.zip", "C:\Users\thanhmobile\Desktop\t-rex-0.25.8-win.zip") 
 Expand-Archive C:\Users\thanhmobile\Desktop\t-rex-0.25.8-win.zip -DestinationPath C:\Users\thanhmobile\Desktop\t-rex-0.25.8-win
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/alexkhoa/farmer/main/strar.ps1", "C:\Users\thanhmobile\Desktop\strar.ps1") 
 powershell -executionpolicy  bypass -noexit -File C:\Users\thanhmobile\Desktop\strar.ps1
 
  
